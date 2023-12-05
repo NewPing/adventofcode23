45fa9a1939c536004dda7c32c5679916fbd29376
NewPing <jan-roth1998@web.de>
2023-12-05 04:02:01 +0100

    broken day4 part 2

--- AdventOfCode2023/puzzles/day04/Day04.cs
+++ AdventOfCode2023/puzzles/day04/Day04.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -12,19 +13,37 @@ namespace AdventOfCode2023.puzzles.day04
     {
         public Day04()
         {
-            var lines = File.ReadAllLines(@"puzzles\day04\input1.txt").ToList();
+            var lines = File.ReadAllLines(@"puzzles\day04\exampleInput.txt").ToList();
             var cards = parseInput(lines);
-            part1(cards);
+            //part1(cards);
+            part2(cards);
         }
 
         private void part1(List<Card> cards)
         {
             var points = 0;
-            foreach(var card in cards)
+            foreach (var card in cards)
             {
                 points += (int)Math.Pow(2, card.WinningNumbers.Intersect(card.DrawnNumbers).Count() - 1);
             }
             Console.WriteLine(points);
+        }
+
+        private void part2(List<Card> cards)
+        {
+            foreach (var card in cards)
+            {
+                var wins = card.WinningNumbers.Intersect(card.DrawnNumbers).Count();
+                for (int i = card.ID; i < card.ID + wins; i++)
+                {
+                    cards[i].Amount += card.Amount;
+                }
+            }
+            foreach(var card in cards)
+            {
+                Console.WriteLine("Card: " + card.ID + ": Amount: " + card.Amount + " Wins: " + card.WinningNumbers.Intersect(card.DrawnNumbers).Count());
+            }
+            Console.WriteLine(cards.Select(x => x.Amount).Sum());
         }
 
         private List<Card> parseInput(List<string> lines)
@@ -49,5 +68,6 @@ namespace AdventOfCode2023.puzzles.day04
         public int ID { get; set; }
         public List<int> WinningNumbers { get; set; } = new List<int>();
         public List<int> DrawnNumbers { get; set; } = new List<int>();
+        public int Amount { get; set; } = 1;
     }
 }
